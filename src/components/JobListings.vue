0f7c649c0c945863150fd765d469e75a40b45650
Sheldon Pasciak <sheldon@pasciak.com>
2025-01-02 11:00:21 -0700

    Misc jobs code progress

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,10 +1,10 @@
 <script setup>
- 
+
 import jobData from '@/jobs.json';
+import { RouterLink } from 'vue-router';
+import JobListing from '@/components/JobListing.vue';
 
-import  JobListing  from '@/components/JobListing.vue';
-
-import { ref,defineProps } from 'vue';
+import { ref, defineProps } from 'vue';
 
 const jobs = ref(jobData);
 
@@ -25,16 +25,14 @@ defineProps({
                 Browse Jobs
             </h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0,limit||jobs.length)" :key="job.id" :job="job"/>
+                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
-        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
+        <RouterLink to="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
+            View All Jobs
+        </RouterLink>
     </section>
 
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
+import NotFound from "@/views/NotFound.vue";
+import JobView from "@/views/JobView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +15,16 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs-view",
       component: JobsView,
+    },
+    {
+      path: "/jobs/:id",
+      name: "job-view",
+      component: JobView,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: NotFound,
     },
   ],
 });
