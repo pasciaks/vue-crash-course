78c4ef42d252219dbda87b8b2fdb7a06a9e49b16
Sheldon Pasciak <sheldon@pasciak.com>
2025-01-02 08:32:20 -0700

    Composition and Options example

---
+++ src/App-OPTIONS-Example.vue
@@ -0,0 +1,55 @@
+
+<!-- OPTIONS API EXAMPLE -->
+
+
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script> -->
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else="status">User is {{status}}</p>
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for google</a> -->
+  <a :href="link">Click for google</a>
+  <!-- <button v-on:click="toggleStatus">Change status</button> -->
+  <button @click="toggleStatus">Change status</button>
+</template>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: "John Doe",
+        status: "something else",
+        tasks: [
+          'task 1','task 2', 'task 3'
+        ],
+        link: 'https://google.com'
+      }
+    },
+    methods: {
+      toggleStatus() {
+        if (this.status==='active') {
+          this.status='pending'
+        } else if (this.status==='pending'){
+          this.status='inactive'
+        } else {
+          this.status='active'
+        }
+      }
+    }
+  }
+</script>
--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<!-- COMPOSITION API EXAMPLE -->
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else="status">User is {{ status }}</p>
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for google</a> -->
+  <a :href="link">Click for google</a>
+  <!-- <button v-on:click="toggleStatus">Change status</button> -->
+  <button @click="toggleStatus">Change status</button>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  color: red;
 }
 </style>
+
+<script setup>
+
+import {ref} from 'vue';
+
+    const name = ref('John Doe');
+    const status = ref('active');
+    const tasks = ref(['one', 'two', 'three']);
+    
+    const toggleStatus = () => {
+      if (status.value === 'active') {
+        status.value= 'pending'
+      } else if (status.value=== 'pending') {
+        status.value= 'inactive'
+      } else {
+        status.value= 'active'
+      }
+    };
+
+</script>
